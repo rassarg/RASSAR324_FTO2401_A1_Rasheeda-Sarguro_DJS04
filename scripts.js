87b4ca6c17d5723f8e1343468d6525d3ad72e7d9
rasheeda <raskieda@gmail.com>
2024-05-13 16:35:17 +0200

    crfeated fn renderAuthors

--- scripts.js
+++ scripts.js
@@ -45,20 +45,23 @@ function renderGenres() {
   document.querySelector("[data-search-genres]").appendChild(genreHtml);
 }
 
-const authorsHtml = document.createDocumentFragment();
-const firstAuthorElement = document.createElement("option");
-firstAuthorElement.value = "any";
-firstAuthorElement.innerText = "All Authors";
-authorsHtml.appendChild(firstAuthorElement);
+// Function to render authors
+function renderAuthors() {
+  const authorsHtml = document.createDocumentFragment();
+  const firstAuthorElement = document.createElement("option");
+  firstAuthorElement.value = "any";
+  firstAuthorElement.innerText = "All Authors";
+  authorsHtml.appendChild(firstAuthorElement);
 
-for (const [id, name] of Object.entries(authors)) {
-  const element = document.createElement("option");
-  element.value = id;
-  element.innerText = name;
-  authorsHtml.appendChild(element);
+  for (const [id, name] of Object.entries(authors)) {
+    const element = document.createElement("option");
+    element.value = id;
+    element.innerText = name;
+    authorsHtml.appendChild(element);
+  }
+
+  document.querySelector("[data-search-authors]").appendChild(authorsHtml);
 }
-
-document.querySelector("[data-search-authors]").appendChild(authorsHtml);
 
 if (
   window.matchMedia &&
